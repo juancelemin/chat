fc765af42bca122c288040f9a95f4f0c2b0d28ac
Juan Guillermo <jgcelemin@indracompany.com>
2021-05-03 21:55:49 -0500

    get user

--- chat-front/src/app/auth/auth.service.ts
+++ chat-front/src/app/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import {HttpClient } from "@angular/common/http";
+import {HttpClient ,HttpParams} from "@angular/common/http";
 
 @Injectable({providedIn:'root'})
 export class AuthService {
@@ -8,12 +8,22 @@ export class AuthService {
 
     signUp(email : String , password : String){
 
-        return this.http.post('http://127.0.0.1:8000/simple/UserDetail',
+        return this.http.post('http://127.0.0.1:8000/login/UserDetail',
         {
             email:email,
             password:password
 
         });
     }
+
+    login(email : string , password : string){
+        let params = new HttpParams().set('email', email).set('password', password);
+
+        return this.http.get('http://127.0.0.1:8000/login/UserDetail',{params:params});
+    }
+    
+    
+
+
 
 }
